eab8b8af6f00d39b229943ff495a0906961a6d5c
Unknown <fanfly1607@163.com>
2017-10-31 20:19:56 +0800

    10.31

--- auto/manage.py
+++ auto/manage.py
@@ -2,7 +2,14 @@
 from bs4 import BeautifulSoup
 import csv
 import os
-import xmlutils
+from xmlutils.xml2csv import xml2csv
+from xmlutils.xml2json import xml2json
+from jtl import create_parser
+from pandas import Series, DataFrame, ExcelWriter
+from openpyxl import load_workbook
+import pandas as pd
+import time
+import re
 
 _this_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -18,5 +25,54 @@ def get_request():
 
 
 def analysis_result():
-    pass
+    converter = xml2csv("log.xml", "log.csv", encoding="utf-8")
+    converter.convert(tag="WHATEVER_GOES_HERE_RENDERS_EMPTY_CSV")
 
+
+def convert_json():
+    converter = xml2json("log.xml", encoding="utf-8")
+    print converter.get_json()
+
+
+def self_convert():
+    fo = open("log.xml", "r")
+    result = fo.read().decode("utf8").encode("GB2312")
+    soup = BeautifulSoup(result, "xml")
+    for each in soup.find_all("sample"):
+        print each
+
+
+def jtl_parse():
+    response_time = []
+    time_stamp = []
+    error_count = 0
+    response = ''
+    parser = create_parser('log.xml')
+    for sample in parser.itersamples():
+        response_time.append(t2s(str(sample.elapsed_time)))
+        time_stamp.append(sample.timestamp)
+        error_count = error_count + sample.error_count
+        response = str(sample.timestamp) + ":" + sample.response_data + "\n"
+    w2e(response_time,time_stamp)
+
+
+def w2e(response_time, time_stamp):
+    f = pd.DataFrame({"response_time": response_time, "time_stamp": time_stamp},
+                     columns=["time_stamp", "response_time"])
+    # f.to_csv("test.csv", index="false") #写入csv
+    # writer = pd.ExcelWriter("test.xlsx", engine='xlsxwriter') #写入xlsx,但是会覆盖
+    book =load_workbook("test.xlsx")
+    writer = pd.ExcelWriter("test.xlsx", engine='openpyxl') #这个引擎不会覆盖
+    writer.book = book
+    writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
+    f.to_excel(writer, sheet_name='bc')
+    writer.save()
+
+
+def t2s(t):
+    x = re.split(':|\.', str(t))
+    time_stamp = (int(x[0]) * 60 * 60 + int(x[1]) * 60 + int(x[2]) + float('0.' + x[3])) * 1000
+    return time_stamp
+
+if __name__ == '__main__':
+    jtl_parse()
---
+++ auto/pd.py
@@ -0,0 +1,37 @@
+# coding=utf-8
+
+from pandas import Series, DataFrame
+import pandas as pd
+import csv
+import re
+
+# s = Series([100, "python", "soochow", "qiwsir"])
+# print s[0]
+
+
+# sd = {"python":800, "c++":8100, "c#":4000}
+# s4 = Series(sd)
+# print s4
+#
+# print pd.isnull(s4)
+#
+# csv_reader = csv.reader(open("log.csv"))
+# for row in csv_reader:
+#     print row
+
+
+# marks = pd.read_csv("log.csv")
+# print marks
+# print marks.columns
+# print marks.index
+
+# data = {"name":["yahoo","google","facebook"],"marks":[200,400,122],"price":[9,3,7]}
+# f1 = DataFrame(data)
+# f1.to_csv("test.csv", index=False, sep=',')
+# print f1
+
+
+t = '0:00:01.171010'
+x = re.split(':|\.',t)
+time_stamp = (int(x[0])*60*60+int(x[1])*60+int(x[2])+float('0.'+x[3]))*1000
+print time_stamp
